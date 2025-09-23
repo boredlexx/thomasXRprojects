745e55e95d484da836fc21c3da87f27873a319a8
boredlexx <at318023@ohio.edu>
2025-09-23 10:48:06 -0400

    room scna nad audio

--- XRI_project/Assets/ThirdParty/_BowAndArrow/Scripts/Bow.cs
+++ XRI_project/Assets/ThirdParty/_BowAndArrow/Scripts/Bow.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using UnityEngine;
+using Oculus.Haptics;
+using UnityEngine.XR.OpenXR.Input;
 
 public class Bow : MonoBehaviour
 {
@@ -20,9 +22,18 @@ public class Bow : MonoBehaviour
 
     public float pullMultiplier = 1;
 
+    [Header("Haptics")]
+    public HapticClipPlayer hapticClipPlayer; //referene for playing haptic studio files
+
+    [Header("Audio")]
+    public AudioClip bowReleaseSound;
+    private AudioSource bowAudioSource;
+
+
     private void Awake()
     {
         m_Animator = GetComponent<Animator>();
+        bowAudioSource = GetComponent<AudioSource>();
     }
 
     private void Start()
@@ -91,6 +102,7 @@ public class Bow : MonoBehaviour
         // If we've pulled far enough, fire
         if (m_PullValue > 0.25f)
             FireArrow();
+        PlayHapticFeedback();
 
         // Clear
         m_PullingHand = null;
@@ -108,5 +120,23 @@ public class Bow : MonoBehaviour
     {
         m_CurrentArrow.Fire(m_PullValue * pullMultiplier);
         m_CurrentArrow = null;
+    }
+
+    private void PlayHapticFeedback()
+    {
+        if (m_PullingHand != null)
+        {
+            HapticSource hapticSource = m_PullingHand.GetComponent<HapticSource>();
+
+            if (hapticSource != null)
+            {
+                hapticSource.Play();
+            }
+        if(bowAudioSource != null && bowReleaseSound != null)
+            {
+                bowAudioSource.PlayOneShot(bowReleaseSound);
+            }
+
+        }
     }
 }
