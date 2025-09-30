b019188dfb612c9a5acf39dda9623b6125100dea
boredlexx <at318023@ohio.edu>
2025-09-30 10:47:17 -0400

    coding and stuff

--- XRI_project/Assets/Scripts/ObjectSpawner.cs
+++ XRI_project/Assets/Scripts/ObjectSpawner.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.XR;
+using UnityEngine.InputSystem.XR; //for commnicating with quest controller
 
 //select objet to spawn
 // where object spawns
@@ -11,20 +13,51 @@ public class ObjectSpawner : MonoBehaviour
 {
     public GameObject objectPrefab; //object to spawn
     public Transform spawnPoint; //where it spawns
-    public XRNode controllerNode = XRNode.RightHand;
+    public XRNode controllerNode = XRNode.RightHand; //assigning controller
     public float spawnCooldown = 1.0f; //need a coroutine
-    private bool canSpawn = true;
-
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private bool canSpawn = true; //time in seconds between spawns
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (canSpawn && IsAButtonPressed())
+        {
+            StartCoroutine(SpawnObjectWithCooldown());
+        }
+    }
+
+    bool IsAButtonPressed()
+    {
+        InputDevice device = InputDevices.GetDeviceAtXRNode(controllerNode);
+        bool buttonPressed = false;
+
+        if (device.TryGetFeatureValue(CommonUsages.triggerButton, out buttonPressed) && buttonPressed) //primary button is "a' or "x" nutton on controller
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    IEnumerator SpawnObjectWithCooldown()
+    {
+        canSpawn = false; //prevents immediate rspawning
+        SpawnObject();
+        yield return new WaitForSeconds(spawnCooldown);
+        canSpawn = true; //allow us to spawn again
+    }
+
+    void SpawnObject()
+    {
+        if(objectPrefab != null && spawnPoint)
+        {
+            GameObject spawnObject = Instantiate(objectPrefab, spawnPoint.position, spawnPoint.rotation);
+        }
+
+        else
+        {
+            Debug.LogError("Assign objectPrefab and spawnPoint in the Inspector");
+        }
+
     }
 }
